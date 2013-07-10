d316a4e814d3e103fde81b88939d288cbe99ea81
Alexey Naydenov <alexey.naydenov@linux.com>
2013-07-10 14:34:03 +0400

    Implement functions for tree traversal and reduction.

--- registergen/test/test_tree.py
+++ registergen/test/test_tree.py
@@ -1,13 +1,22 @@
+from pprint import pprint
+
 import registergen.tree as rgt
 
+TEST_REGISTER_TREE = {'sections': [{'addr': '0x02530000',
+                                    'desc': 'I2C configuration registers',
+                                    'name': 'i2c',
+                                    'registers': [{'desc': 'I2C Mode Register',
+                                                   'name': 'ICMDR',
+                                                   'off': '0x24'}]}]}
 
-{'sections': [{'addr': '0x02530000',
-   'desc': 'I2C configuration registers',
-   'name': 'i2c',
-   'registers': [{'desc': 'I2C Mode Register',
-     'name': 'ICMDR',
-     'off': '0x24'}]}]}
+def test_transform_tree_identity():
+    assert rgt.transform_tree(rgt.identity_transform, 
+                              TEST_REGISTER_TREE) == TEST_REGISTER_TREE
 
+# def test_accumulate_paths():
+#     print(rgt.reduce_tree(rgt.accumulate_paths, TEST_REGISTER_TREE)[0])
 
-def test_nothing():
-    assert True
+def test_convert_numeric_values():
+    pprint(TEST_REGISTER_TREE)
+    pprint(rgt.transform_tree(rgt.convert_to_numbers, TEST_REGISTER_TREE, 
+                             rgt.numeric_filter))
--- registergen/tree.py
+++ registergen/tree.py
@@ -20,19 +20,73 @@
 
 """Different utilities for walking register description tree."""
 
+from collections import abc
+
+import registergen.utils as rgu
+
 _SECTIONS = 'sections'
 _REGISTERS = 'registers'
 _FIELDS = 'fields'
 _VALUES = 'values'
-_CHILDREN_KEY_NAMES = [_SECTIONS, _REGISTERS, _FIELDS, _VALUES]
-_NUMERIC_KEY_NAMES = ['addr', 'off', 'val']
+_CHILDREN_KEY_NAMES = set([_SECTIONS, _REGISTERS, _FIELDS, _VALUES])
+_NUMERIC_KEY_NAMES = set(['addr', 'off', 'val'])
 
-def always_apply_condition(path, key):
+def apply_to_all(path):
     """Helper function that applies to all nodes."""
     return True
 
-def apply_to_leaves(function, tree, condition=None):
-    """Apply a function to values of nodes that satisfy some condition. """
-    
+def identity_transform(path, value):
+    """Does not do anything with the tree, test function."""
+    return value
 
+def identity_transform_debug(path, value):
+    print(path)
+    return value
+
+def numeric_filter(path):
+    return path[-1] in _NUMERIC_KEY_NAMES
+
+def convert_to_numbers(path, value):
+    return rgu.string_to_int(value)
+
+def transform_tree(function, tree, condition=None, path=None):
+    """Apply a function to values of nodes that satisfy some condition.
+
+    function must take a path value pair and return a new value;
+    condition takes a path and returns a bool that indicates whether 
+    the function must be applied to the given node.
+    """
+    if condition is None:
+        condition = apply_to_all
+    if path is None:
+        path = []
+    # transform breath first way, so that functions can remove subtrees
+    for k, v in tree.items():
+        if condition(path + [k]):
+            tree[k] = function(path + [k], v)
+    # iterate over children
+    for k, v in tree.items():
+        if k in _CHILDREN_KEY_NAMES and isinstance(v, abc.Iterable):
+            tree[k] = [transform_tree(function, c, condition, path + [k]) 
+                       for c in v]
     return tree
+
+def accumulate_paths(path, value, accumulator):
+    """Accumulate paths of visited nodes."""
+    if accumulator is None:
+        accumulator = []
+    accumulator.append(".".join(path))
+    return value, accumulator        
+
+def reduce_tree(function, tree, accumulator=None):
+    """Walk the tree and accumulate some data from it.
+
+    function takes path, value and current value of the accumulator,
+    it must return new value and new accumulator.
+    """
+    def transform_function(path, value):
+        nonlocal accumulator
+        new_value, accumulator = function(path, value, accumulator)
+        return new_value
+    tree = transform_tree(transform_function, tree)
+    return accumulator, tree
